38405eb25f385eb4ccb8b42f1d09ec1048af5c3c
Administrator <352913998@qq.com>
2019-06-27 11:31:24 +0800

    ocr

--- app/src/main/java/com/packagename/ocr/MainActivity.java
+++ app/src/main/java/com/packagename/ocr/MainActivity.java
@@ -1,7 +1,10 @@
 package com.packagename.ocr;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -358,6 +361,25 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
         }
+    }
+
+   //判断网络是否可用
+    public boolean isNetworkAvailable(){
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) {
+        } else {
+           //如果仅仅是用来判断网络连接
+          //则可以使用 cm.getActiveNetworkInfo().isAvailable();
+            NetworkInfo[] info = cm.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     @Override
